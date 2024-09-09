5edfdd59d8b69c2b1962f862a02d657f03dddf31
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-09-09 13:25:24 +0300

    summary

--- src/Whoosh.java
+++ src/Whoosh.java
@@ -1,10 +1,25 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Whoosh {
     public static void main(String[] args) {
-        String teachers = "fgrdhfdhfhfhxfgh";
-        String teacher = "teachers: ";
-        System.out.println(teacher + teachers);
+       String isMugle = """
+               Yes 
+               But
+               """;
+       String isTeacher = "true";
+        System.out.println(isMugle.indexOf("Yes"));
+        castSpell();
 
+    }
+    public static void castSpell() {
+    String spell =  "ARDENTIS VERUM LUMINOS ET FULGUR SYLVESTRA ELIXIA";
+    String spellToLow = spell.toLowerCase();
+    int start = new Random().nextInt(0,50);
+    String substring = spellToLow.substring(start);
+    StringBuilder spellSub = new StringBuilder(substring);
+    spellSub.reverse();
+        System.out.println(spellSub);
     }
 }
